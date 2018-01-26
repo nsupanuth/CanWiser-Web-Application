9d43357ec6f42b37a71c9c0cb6dc128fc4d41219
Sakan Komolvatin <tutumm96@gmail.com>
2018-01-26 14:08:47 +0700

    Upload design

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,115 +1,98 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+
+import Profile from './img/profile.jpg';
+
+const Nav = styled.div`
+
+font-family: 'Montserrat', sans-serif;
+
+.bg-dark {
+  background-color: #43bfc7 !important;
+}
+
+#mainNav.navbar-dark .navbar-collapse .navbar-sidenav {
+  background: #43bfc7;
+}
+
+.nav-link-text {
+  color: white !important;
+}
+
+.fa {
+  color: white;
+}
+
+#mainNav.fixed-top.navbar-dark .sidenav-toggler {
+  background-color: #43bfc7;
+}
+
+.fa-angle-left {
+  color: transparent !important;
+}
+
+.fa-angle-right {
+  color: white !important;
+}
+
+.profile-img img{
+  width: 60%;
+}
+
+.profile-img {
+  text-align: center;
+}
+`
 
 class Navbar extends Component {
   render() {
     return (
-      <div>
+      <Nav>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-        <a class="navbar-brand" href="/">CanWiser</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-              <a class="nav-link" href="/">
-                <i class="fa fa-fw fa-dashboard"></i>
-                <span class="nav-link-text">Dashboard</span>
-              </a>
+          <a class="navbar-brand" href="/">CanWiser</a>
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+
+            <li class="nav-item profile-img" data-toggle="tooltip" data-placement="right" title="Dashboard">
+                <img class="rounded-circle" alt="profile" src={Profile} />
             </li>
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
-              <a class="nav-link" href="/upload">
-                <i class="fa fa-fw fa-area-chart"></i>
-                <span class="nav-link-text">Upload</span>
-              </a>
-            </li>
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
-              <a class="nav-link" href="tables.html">
-                <i class="fa fa-fw fa-table"></i>
-                <span class="nav-link-text">Tables</span>
-              </a>
-            </li>
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-              <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
-                <i class="fa fa-fw fa-wrench"></i>
-                <span class="nav-link-text">Components</span>
-              </a>
-              <ul class="sidenav-second-level collapse" id="collapseComponents">
-                <li>
-                  <a href="navbar.html">Navbar</a>
-                </li>
-                <li>
-                  <a href="cards.html">Cards</a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
-              <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
-                <i class="fa fa-fw fa-file"></i>
-                <span class="nav-link-text">Example Pages</span>
-              </a>
-              <ul class="sidenav-second-level collapse" id="collapseExamplePages">
-                <li>
-                  <a href="login.html">Login Page</a>
-                </li>
-                <li>
-                  <a href="register.html">Registration Page</a>
-                </li>
-                <li>
-                  <a href="forgot-password.html">Forgot Password Page</a>
-                </li>
-                <li>
-                  <a href="blank.html">Blank Page</a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
-              <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
-                <i class="fa fa-fw fa-sitemap"></i>
-                <span class="nav-link-text">Menu Levels</span>
-              </a>
-              <ul class="sidenav-second-level collapse" id="collapseMulti">
-                <li>
-                  <a href="#">Second Level Item</a>
-                </li>
-                <li>
-                  <a href="#">Second Level Item</a>
-                </li>
-                <li>
-                  <a href="#">Second Level Item</a>
-                </li>
-                <li>
-                  <a class="nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti2">Third Level</a>
-                  <ul class="sidenav-third-level collapse" id="collapseMulti2">
-                    <li>
-                      <a href="#">Third Level Item</a>
-                    </li>
-                    <li>
-                      <a href="#">Third Level Item</a>
-                    </li>
-                    <li>
-                      <a href="#">Third Level Item</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
-              <a class="nav-link" href="#">
-                <i class="fa fa-fw fa-link"></i>
-                <span class="nav-link-text">Link</span>
-              </a>
-            </li>
-          </ul>
-          <ul class="navbar-nav sidenav-toggler">
-            <li class="nav-item">
-              <a class="nav-link text-center" id="sidenavToggler">
-                <i class="fa fa-fw fa-angle-left"></i>
-              </a>
-            </li>
-          </ul>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown">
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+                <a class="nav-link" href="/">
+                  <i class="fa fa-fw fa-dashboard"></i>
+                  <span class="nav-link-text">Dashboard</span>
+                </a>
+              </li>
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+                <a class="nav-link" href="/upload">
+                  <i class="fa fa-fw fa-area-chart"></i>
+                  <span class="nav-link-text">Upload</span>
+                </a>
+              </li>
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+                <a class="nav-link" href="tables.html">
+                  <i class="fa fa-fw fa-table"></i>
+                  <span class="nav-link-text">Prediction</span>
+                </a>
+              </li>
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
+                <a class="nav-link" href="#">
+                  <i class="fa fa-fw fa-link"></i>
+                  <span class="nav-link-text">Link</span>
+                </a>
+              </li>
+            </ul>
+            <ul class="navbar-nav sidenav-toggler">
+              <li class="nav-item">
+                <a class="nav-link text-center" id="sidenavToggler">
+                  <i class="fa fa-fw fa-angle-left"></i>
+                </a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+              {/* <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fa fa-fw fa-envelope"></i>
                 <span class="d-lg-none">Messages
@@ -185,27 +168,15 @@ class Navbar extends Component {
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item small" href="#">View all alerts</a>
               </div>
-            </li>
-            <li class="nav-item">
-              <form class="form-inline my-2 my-lg-0 mr-lg-2">
-                <div class="input-group">
-                  <input class="form-control" type="text" placeholder="Search for..." />
-                  <span class="input-group-btn">
-                    <button class="btn btn-primary" type="button">
-                      <i class="fa fa-search"></i>
-                    </button>
-                  </span>
-                </div>
-              </form>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
-                <i class="fa fa-fw fa-sign-out"></i>Logout</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      </div>
+            </li> */}
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+                  <i class="fa fa-fw fa-sign-out"></i>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </Nav>
     )
   }
 }
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,6 +1,58 @@
 import React, { Component } from 'react';
-
 import axios from 'axios'
+import styled from 'styled-components';
+
+const ShowUpload = styled.div`
+
+  text-align: center;
+  font-family: 'Montserrat', sans-serif;
+
+  h1 {
+    color: #939393;
+  }
+
+  label {
+    border: 2px solid #46b6a6;
+    cursor: pointer;
+    color: #46b6a6;
+    border-radius: 60px;
+    height: 30px;
+    width: 150px;
+  }
+
+  .btn-success {
+    color: #fff;
+    background-color: #46b6a6;
+    border-color: #46b6a6;
+    width: 150px;
+    height: 30px;
+    border-radius: 50px;
+    cursor: pointer;
+}
+`
+
+const ShowScore = styled.p`
+  color: #46b6a6;
+  text-align: center;
+  font-family: 'Montserrat', sans-serif;
+  margin: auto;
+  display: inline-block;
+  margin: 0px 20px;
+
+  .score {
+    font-size: 25px;
+    height: 150px;
+    width: 150px;
+    border: 3px solid #46b6a6;
+    border-radius: 75px;
+    padding-top: 55px !important;
+  }
+
+  .text {
+    font-weight: bold;
+    font-size: 20px;
+  }
+`
 
 class Home extends Component {
 
@@ -35,7 +87,8 @@ class Home extends Component {
         console.log(res)
         this.setState({
           results: res.data.results,
-          uploadStatus: 'Upload'
+          uploadStatus: 'Upload',
+          filename: ''
         })
       })
   }
@@ -44,12 +97,12 @@ class Home extends Component {
     axios.post('http://13.229.135.29:8080/retrain/upload/cancel', {
       pathName: this.state.results.modelPath
     })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   handleModelSave() {
@@ -60,19 +113,38 @@ class Home extends Component {
       model_name: this.state.results.modelName,
       model_path: this.state.results.modelPath
     })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
 
     return (
-      <div className="box">
-        <p> {this.state.filename} </p>
+      <div>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item active">Upload</li>
+        </ol>
+
+        <ShowUpload>
+          <h1 style={this.state.results ? {marginTop: 100} : {marginTop: 200}}>Upload Your Data Here !</h1>
+          {/* <ShowScore><p class="score">0.92</p><p class="text"></p></ShowScore> */}
+          {this.state.results ? <ShowScore><p class="score">{this.state.results ? this.state.results.f1.toFixed(2) : ''}</p><p class="text">{this.state.results ? 'F1' : ''}</p></ShowScore> : ''}
+          {this.state.results ? <ShowScore><p class="score">{this.state.results ? this.state.results.accuracy.toFixed(2) : ''}</p><p class="text">{this.state.results ? 'Accuracy' : ''}</p></ShowScore> : ''}
+          {this.state.results ? <ShowScore><p class="score">{this.state.results ? this.state.results.recall.toFixed(2) : ''}</p><p class="text">{this.state.results ? 'Recall' : ''}</p></ShowScore> : ''}
+          {this.state.results ? <ShowScore><p class="score">{this.state.results ? this.state.results.modelName : ''}</p><p class="text">{this.state.results ? 'Model Name' : ''}</p></ShowScore> : ''}
+          {/* {this.state.results ? <ShowScore><p class="score">{this.state.results ? this.state.results.features : ''}</p><p class="text">{this.state.results ? 'Features' : ''}</p></ShowScore> : ''} */}
+          <form ref="myForm">
+            <input ref="myFile" name="xxx" id="file" onChange={() => this.handleFileChange()} type="file" style={{ display: 'none' }} />
+            <label for="file"><span style={{ marginTop: 100 }}>Choose a file</span></label> {this.state.filename}
+          </form>
+          {this.state.filename ? <button onClick={() => this.handleFileUpload()} className="button btn-success">{this.state.uploadStatus}</button> : ''}
+        </ShowUpload>
+
+        {/* <p> {this.state.filename} </p>
         <form ref="myForm">
           <input ref="myFile" name="xxx" onChange={() => this.handleFileChange()} type="file" />
         </form>
@@ -81,11 +153,10 @@ class Home extends Component {
         <p>{this.state.results ? 'Accuracy: ' : ''} {this.state.results ? this.state.results.accuracy : ''}</p>
         <p>{this.state.results ? 'Features: ' : ''} {this.state.results ? this.state.results.features : ''}</p>
         <p>{this.state.results ? 'Model Name: ' : ''} {this.state.results ? this.state.results.modelName : ''}</p>
-        {/* <p>{this.state.results ? this.state.results.modelPath : ''}</p> */}
         <p>{this.state.results ? 'Recall: ' : ''} {this.state.results ? this.state.results.recall : ''}</p>
 
         {this.state.results ? <button onClick={() => this.handleModelCancle()} className="button btn-danger">Cancle</button>: ''}
-        {this.state.results ? <button onClick={() => this.handleModelSave()} className="button btn-primary">Save</button>: ''}
+        {this.state.results ? <button onClick={() => this.handleModelSave()} className="button btn-primary">Save</button>: ''} */}
 
       </div>
     )
